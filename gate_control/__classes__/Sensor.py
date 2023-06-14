7149a398ad66c43c989280f102c332efe60e8328
meow <mark.styx@hotmail.com>
2023-06-13 21:26:49 -0400

    updates

--- gate_control/__classes__/Sensor.py
+++ gate_control/__classes__/Sensor.py
@@ -2,9 +2,12 @@ from gate_control import GPIO
 
 class Sense:
 
-    def __init__(self,gpio) -> None:
+    def __init__(self,gpio,id) -> None:
+        self.id = id
         self.gpio = gpio
         GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def get_state(self):
-        return GPIO.input(self.gpio)
+        state = GPIO.input(self.gpio)
+        if state:
+            print(f'sensed {self.id}')
--- gate_control/__classes__/Switch.py
+++ gate_control/__classes__/Switch.py
@@ -5,22 +5,27 @@ PULSE = 1
 DELAY = .5
 class Relay:
 
-    def __init__(self,gpio) -> None:
+    def __init__(self,gpio,id) -> None:
+        self.id = id
         self.gpio = gpio
         GPIO.setup(self.gpio['ON'], GPIO.OUT)
         GPIO.setup(self.gpio['OFF'], GPIO.OUT)
 
-    
-    def close(self):
+    def _reset(self):
+        print(f'relay {self.id} reset')
         GPIO.output(self.gpio['OFF'], 0)
-        sleep(DELAY)
-        GPIO.output(self.gpio['ON'], 1)
-        sleep(PULSE)
         GPIO.output(self.gpio['ON'], 0)
+        sleep(DELAY)
+    
+    def _pulse(self,switch):
+        print(f'pulse {self.id} {switch}')
+        self._reset()
+        GPIO.output(self.gpio[switch],1)
+        sleep(PULSE)
+        GPIO.output(self.gpio[switch],0)
+
+    def close(self):
+        self._pulse('ON')
 
     def open(self):
-        GPIO.output(self.gpio['ON'], 0)
-        sleep(DELAY)
-        GPIO.output(self.gpio['OFF'], 1)
-        sleep(PULSE)
-        GPIO.output(self.gpio['OFF'], 0)
+        self._pulse['OFF']
