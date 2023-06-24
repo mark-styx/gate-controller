f9ae64aa2d75d8663ee76c37364ac41936ecf781
meow <mark.styx@hotmail.com>
2023-06-23 21:46:11 -0400

    adding script to trigger all door processes

--- helms-deep.sh
+++ helms-deep.sh
@@ -1,3 +1,4 @@
+cd /home.gate/gate-controller
 nohup python3 -m gate_control.doorman -m 0
 nohup python3 -m gunicorn -w 4 -b 0.0.0.0 'gate_control.gate_api:app'
 nohup python3 -m gate_control.button_sense -m 0
