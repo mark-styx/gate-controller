2f70f9e5217a21df3c9674b5d23a7f70ca31564a
mark <m.styx@frameplay.gg>
2023-07-22 21:44:13 -0400

    adding stuff

--- gate_control/button_sense.py
+++ gate_control/button_sense.py
@@ -1,7 +1,7 @@
 from gate_control import REVERE
-from gate_control.config import CADENCE
 from gate_control.config import SENSORS
 from gate_control.__classes__.Sensor import Sense
+from gate_control.__classes__.Events import event
 
 from datetime import datetime as dt
 from time import sleep
@@ -11,61 +11,47 @@ import argparse
 Button = Sense(gpio=SENSORS['GPIO']['BT'],id='momentary switch')
 ts = lambda:dt.now().timestamp()
 
-def __opposite_direction__(current_state):
-   travel = {
-        'UP':'DN'
-      , 'DN':'UP'
-      , 'Opening':'DN'
-      , 'Closing':'UP'
-   }
-   target = travel.get(current_state)
-   if not target:
-      target = REVERE.get('task')
-   return target
+
+def epoch(
+         state:int=0
+        ,mock:bool=False
+):
+    return {
+         'state':state
+        ,'mock':mock
+    }
+
+
+
+def eval_history(hist:dict)->dict:
+    keys = list(hist.keys())
+    keys.sort()
+    keys = keys[-30:]
+    current_activations = [x for x in keys if x >= ts() - 1]
+    prev_activations = [x for x in hist if x.get('state') and x not in current_activations]
+    action_space = {
+          'ebrake':len(current_activations+prev_activations) == 30
+        , 'partial_activation':prev_activations and current_activations and ((min(current_activations) - max(prev_activations)) >= 1)
+        , 'activation':not(prev_activations) and current_activations
+    }
+    e = [k for k,v in action_space if v].pop()
+    action = event(action=e)
+    action.add_event()
+    return {k:v for k,v in hist if k in keys}
+
 
 def button_control_flow(mock=False):
     '''
     Check the state of the momentary switch at a defined cadence.
     If activated, sends message to the doorman and waits for feedback.
     '''
-    hist = []
-    t = dt.now().timestamp()
+    hist = {}
     while True:
-        if Button.get_state() and (ts() - t >= 1):
-            if mock:
-                print('mock button activation')
-            else:
-                hist.append(1)
-                t = ts()
-                cstate,ct = REVERE.mget(["state","t"])
-                if t - float(ct) < 1:
-                    msg = 'Not enough time between requests, ignoring'
-                    print(msg)
-                else:
-                    task = __opposite_direction__(cstate)                    
-                    if task:
-                        REVERE.mset({"task":task,"t":ts()})
-                    else:
-                        print('Error: Improper Target')
-                        continue
-                    while cstate not in ['Opening','Closing']:
-                        cstate = REVERE.get("state")
-                        sleep(CADENCE)
-        elif Button.get_state() and not (ts() - t >= 1):
-            if mock:
-                print('mock button duplicate activation')
-            else:
-                hist.append(1)
-                msg = 'Not enough time between button senses, ignoring'
-                print(msg)
+        if Button.get_state():
+            hist[ts()] = epoch(state=1,mock=mock)
         else:
-            hist.append(0)
-        hist = hist[:30]   
-        if sum(hist) == 30:
-            print('triggering ebrake')
-            ebrake = REVERE.get('ebrake')
-            new = {'ON':'OFF','OFF':'ON'}[ebrake]
-            REVERE.mset({'ebrake':new,'ebrake_eid':ts()})
+            hist[ts()] = epoch(mock=mock)
+        hist = eval_history(hist)
         sleep(SENSORS['PING'])
 
 
