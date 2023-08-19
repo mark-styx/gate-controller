c2dcbe3cf02ddb4b73268f384d595a5dddeca084
meow <mark.styx@hotmail.com>
2023-08-19 15:03:59 -0400

    added press minimum threshold

--- gate_control/button_sense.py
+++ gate_control/button_sense.py
@@ -3,7 +3,7 @@
 # This means that the history eval should be modified to a list.
 # Which makes some of the filtering methods unnecessary.
 
-from gate_control.config import SENSORS,SWITCH_EBRAKE
+from gate_control.config import SENSORS,SWITCH_EBRAKE,SWITCH_THRESHOLD
 from gate_control.__classes__.Sensor import Sense
 from gate_control.__classes__.Events import event
 from gate_control import GPIO,log
@@ -16,97 +16,6 @@ import argparse
 Button = Sense(gpio=SENSORS['GPIO']['BT'],id='momentary switch',active=0)
 ts = lambda:dt.now().timestamp()
 
-def epoch(
-         state:int=0
-        ,mock:bool=False
-):
-    return {
-         'state':state
-        ,'mock':mock
-        ,'triggered':False
-    }
-
-def filter_history(hist:dict,method:str):
-    keys = sorted(list(hist.keys()))
-    t = ts()
-    return {
-         'current':lambda:{k:v for k,v in hist.items() if (t-k) <= 1}
-        ,'previous':lambda:{k for k,v in hist.items() if v['triggered'] and t-k <= 1}
-        ,'last_30':lambda:{k:v for k,v in hist.items() if k in keys[-30:]}
-    }[method]()
-
-def get_activations(hist:dict):
-    return [k for k,v in hist.items() if v['state'] == 1]
-
-def get_current_activations(hist:dict):
-    current = filter_history(hist,'current')
-    return get_activations(current)
-
-def get_prev_activations(hist:dict):
-    previous = filter_history(hist,'previous')
-    return len(previous) > 1
-
-def get_all_activations(hist:dict):
-    last_30 = filter_history(hist,'last_30')
-    return get_activations(last_30)
-
-def action_triage(current:list,last_30:list,prev:bool):
-    actions = {
-          'ebrake':len(last_30) == 30
-        , 'activate':not(len(last_30) == 30) and current and not prev
-    }
-    action_determination = [k for k,v in actions.items() if v]
-    assert(len(action_determination) <= 1)
-    if action_determination:
-        return action_determination.pop()
-
-def _eval_history(hist:dict)->dict:
-    '''
-    params:
-        hist:dict - The dictionary containing the historical data for the button sense iterator.
-    Takes the historical data and evaluates what actions if any should occur as a result of the event history and outputs a truncated version of the history.
-
-    returns:
-        hist:dict
-    '''
-
-    action = action_triage(
-         current=get_current_activations(hist)
-        ,last_30=get_all_activations(hist)
-        ,prev=get_prev_activations(hist)
-    )
-    if action:
-        if not any([x['mock'] for x in hist.values()]):
-            event(action=action)
-            hist[list(hist.keys())[-1]]['triggered']=True
-        else:
-            print(f'mock {action}')
-    return filter_history(hist,'last_30')
-
-
-def trunc_history(hist:dict)->dict:
-    keys = sorted(list(hist.keys()))[-30:]
-    hist = {k:v for k,v in hist.items() if k in keys}
-    return hist
-    
-    
-def eval_history(hist:dict,last_activation,mock)->dict:
-    ebrake = all([v['state'] == 1 for v in hist.values()])
-    if ebrake:
-        event('ebrake',MOCK=mock)
-    elif ts() - last_activation >= 1:
-        event('activate',MOCK=mock)
-    else:
-        pass
-
-# Instead of stream, consider batches of pulses
-# Pulase Batch -> Evaluate -> Send
-# Wait until command sequence has finished before sending
-# Ex. If two pulses return True, then keep listening for the third one
-# Pulse should return true if any of the checks return True during the pulse.
-
-# Maybe Just check how long it was held?
-
 
 def command_sequence(start):
     log('button_sense',3,'command sequence enter')
@@ -117,9 +26,11 @@ def command_sequence(start):
     if (end - start) >= SWITCH_EBRAKE:
         log('button_sense',3,'ebrake resolved')
         return 'ebrake'
-    else:
+    elif (end-start) >= SWITCH_THRESHOLD:
         log('button_sense',3,'activation resolved')
         return 'activate'
+    else:
+        log('button_sense',3,'suspected false positive: press did not meet threshold!')
 
 def button_control_flow(mock=False):
     log('button_sense',3,'button control flow enter')
@@ -131,24 +42,6 @@ def button_control_flow(mock=False):
             event(action,source='button',MOCK=mock)
         sleep(SENSORS['PING'])
 
-
-def _button_control_flow(mock=False):
-    '''
-    Check the state of the momentary switch at a defined cadence.
-    If activated, sends message to the doorman and waits for feedback.
-    '''
-    hist = {}
-    last_activation = 0
-    while True:
-        if Button.get_state():
-            print('sensed')
-            hist.update({ts():epoch(state=1,mock=mock)})
-            eval_history(hist,last_activation,mock)
-            last_activation = ts()
-        else:
-            hist.update({ts():epoch(state=0,mock=mock)})
-        hist = trunc_history(hist)
-        sleep(SENSORS['PING'])
 
 
 if __name__ == '__main__':
